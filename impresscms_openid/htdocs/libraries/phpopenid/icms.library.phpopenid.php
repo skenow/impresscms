0421965a349423cb47dcb30f3c9f418e812d81e5
Marc-André Lanciault <marcan@impresscms.org>
2008-02-16 16:50:35 +0000

    Finishing the addition of the php-openid library

---
+++ impresscms_openid/htdocs/libraries/phpopenid/icms.library.phpopenid.php
@@ -0,0 +1,23 @@
+<?
+/**
+* Initiating file for the third party library php-openid
+*
+* This file is responsible for initiating the php-openid library within ImpressCMS
+*
+* @copyright	The ImpressCMS Project http://www.impresscms.org/
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @package		libraries
+* @since		1.1
+* @author		marcan <marcan@impresscms.or>
+* @version		$Id$
+* @link 		http://openidenabled.com/php-openid/ php-openid library from OpenID Enabled
+*/
+
+/*
+function icmsLibraryPhpopenid_beforeFooter() {
+	global $xoTheme;
+	$xoTheme->addScript(ICMS_LIBRARIES_URL . '/scriptaculous/lib/prototype.js');
+	$xoTheme->addScript(ICMS_LIBRARIES_URL . '/scriptaculous/src/scriptaculous.js');
+}
+*/
+?>
