70fe1b3319bafa57d414970bac6eaecf8812bc2a
Rodrigo Pereira Lima <therplima@impresscms.org>
2008-02-24 22:04:31 +0000

    improve the preview script

--- htdocs/modules/system/admin/images/preview.php
+++ htdocs/modules/system/admin/images/preview.php
@@ -4,5 +4,13 @@ include("../../../../libraries/wideimage/lib/WideImage.inc.php");
 header('Content-type: image/png');
 $file = $_GET['file'];
 //echo XOOPS_ROOT_PATH.'/uploads/'.$file;
-echo wiImage::load(XOOPS_ROOT_PATH.'/uploads/'.$file)->resize(400, 300)->asString('png');
+$img = wiImage::load(XOOPS_ROOT_PATH.'/uploads/'.$file);
+$width = $img->getWidth();
+$height = $img->getHeight();
+if ($width > 400 || $height > 300){
+    echo $img->resize(400, 300)->asString('png');	
+}else{
+	echo $img->asString('png');
+}
+
 ?>
