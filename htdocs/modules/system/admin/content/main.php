20e4f8f8c739fbbbdcf073d753b59522fec412e2
Rodrigo Pereira Lima <therplima@impresscms.org>
2008-07-30 18:42:26 +0000

    First step of the fix for the bug item 2032094 - 1.1 redirect bug

--- htdocs/modules/system/admin/content/main.php
+++ htdocs/modules/system/admin/content/main.php
@@ -24,6 +24,8 @@ if ( !is_object($xoopsUser) || !is_object($xoopsModule) || !$xoopsUser->isAdmin(
     $limit = (isset($_GET['limit']))?intval($_GET['limit']):((isset($_POST['limit']))?intval($_POST['limit']):15);
     $start = (isset($_GET['start']))?intval($_GET['start']):((isset($_POST['start']))?intval($_POST['start']):0);
     $editor = (isset($_GET['editor']))?$_GET['editor']:null;
+    $canceled = (isset($_GET['canceled']))?intval($_GET['canceled']):((isset($_POST['canceled']))?intval($_POST['canceled']):0);
+    
     switch ($op){
     	case 'list':
     		xoops_cp_header();
@@ -53,9 +55,7 @@ if ( !is_object($xoopsUser) || !is_object($xoopsModule) || !$xoopsUser->isAdmin(
     		contmanager_clonecontent($content_id);
     		break;
     	case 'delcontent':
-    		xoops_cp_header();
-    		xoops_confirm(array('op' => 'delcontentok', 'content_id' => $content_id, 'fct' => 'content'), 'admin.php', _MD_RUDELCONTENT);
-    		xoops_cp_footer();
+    		contmanager_confirmdelcontent($content_id);
     		break;
     	case 'delcontentok':
     		contmanager_delcontent($content_id);
@@ -339,6 +339,40 @@ function contmanager_delcontent($content_id) {
 	}
 
 	redirect_header('admin.php?fct=content',2,_MD_AM_DBUPDATED);
+}
+
+function contmanager_confirmdelcontent($content_id){
+	global $canceled;
+	
+	if ($canceled){
+		header('Location: '.ICMS_URL.'/modules/system/admin.php?fct=content');
+	}
+	
+	$content_handler =& xoops_gethandler('content');
+	$content = $content_handler->get($content_id);
+	
+	$content_url = 'content.php?page='.$content_handler->makeLink($content);
+
+	$page_handler = xoops_gethandler('page');
+	$criteria = new CriteriaCompo(new Criteria('page_url', $content_url));
+	$criteria->add(new Criteria('page_url', ICMS_URL.'/'.$content_url),'OR');
+	$pages = $page_handler->getObjects($criteria);
+
+	if (count($pages) > 0){
+		$page = $pages[0];
+	}else{
+		$page = false;
+	}
+
+	$redir = base64_encode('admin.php?fct=content&op=delcontent&content_id='.$content_id);
+	
+	if (!$page || !is_object($page)){
+		xoops_cp_header();
+		xoops_confirm(array('op' => 'delcontentok', 'content_id' => $content_id, 'fct' => 'content'), 'admin.php', _MD_RUDELCONTENT);
+		xoops_cp_footer();
+	}else{
+		redirect_header('admin.php?fct=pages&op=delpage&page_id='.$page->getVar('page_id').'&redir='.$redir,5,_MD_DELLINKFIRST);
+	}
 }
 
 function contmanager_changestatus($content_id) {
