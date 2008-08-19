b945f249c2f212348d3090e6992a0f473c56a59c
Sina Asghari <stranger@impresscms.org>
2008-08-19 23:50:14 +0000

    typo

--- htdocs/libraries/captcha/config.php
+++ htdocs/libraries/captcha/config.php
@@ -12,7 +12,7 @@
  * @copyright	XOOPS_copyrights.txt
  * @copyright	http://www.impresscms.org/ The ImpressCMS Project
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
- * @package		installer
+ * @package		libraries
  * @since		XOOPS
  * @author		http://www.xoops.org/ The XOOPS Project
  * @author		Taiwen Jiang (phppp or D.J.) <php_pp@hotmail.com>
@@ -24,7 +24,7 @@ $config = array(
 	"mode"				=> 'image', 
 	"name"				=> 'xoopscaptcha', 
 	"skipmember"		=> false,					// Skip CAPTCHA check for members
-	"maxattempt"		=> 4848,  					// Maximum attempts for each session
+	"maxattempt"		=> 8,  					// Maximum attempts for each session
 	
 	"num_chars"			=> 4,  						// Maximum characters
 	
@@ -33,8 +33,8 @@ $config = array(
 	"imagepath"			=> "cache/captcha",		// Path to temporary image files, __relative__ to ICMS_ROOT_PATH
 	"imageurl"			=> "libraries/captcha/scripts/img.php",		// Path to the script for creating image, __relative__ to ICMS_ROOT_PATH
 	"casesensitive"		=> false,					// Characters in image mode is case-sensitive
-	"fontsize_min"		=> 12,  					// Minimum font-size
-	"fontsize_max"		=> 14,  					// Maximum font-size 
+	"fontsize_min"		=> 9,  					// Minimum font-size
+	"fontsize_max"		=> 10,  					// Maximum font-size 
 	"background_type"	=> 100, 						// Background type in image mode: 0 - bar; 1 - circle; 2 - line; 3 - rectangle; 4 - ellipse; 5 - polygon; 100 - generated from files
 	"background_num"	=> 50,						// Number of background images to generate
 	"polygon_point"		=> 3,
