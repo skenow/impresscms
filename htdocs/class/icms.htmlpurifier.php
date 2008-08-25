6af5e9f181378b091ad870c6908cea82a51a6656
Steve Kenow <skenow@impresscms.org>
2008-08-25 18:00:44 +0000

    Removed line break in $HTML_Allowed_Attr to eliminate warnings in icms.htmlpurifier.php. For some odd reason, the line break was being added as an empty attribute

--- htdocs/class/icms.htmlpurifier.php
+++ htdocs/class/icms.htmlpurifier.php
@@ -136,8 +136,7 @@ class icms_HTMLPurifier
 		
 		$HTML_Allowed_Elms = 'a, abbr, acronym, b, blockquote, br, caption, cite, code, dd, del, dfn, div, dl, dt, em, font, h1, h2, h3, h4, h5, h6, i, img, ins, kbd, li, ol, p, pre, s, span, strike, strong, sub, sup, table, tbody, td, tfoot, th, thead, tr, tt, u, ul, var';
 
-		$HTML_Allowed_Attr = 'a.href, a.title, a.target, a.rel, abbr.title, acronym.title, blockquote.cite, div.align, div.style, div.class, font.size, font.color,
-		h1.style, h2.style, h3.style, h4.style, h5.style, h6.style, img.src, img.alt, img.title, img.class, img.align, img.style, p.style, span.style, span.class, table.class, table.border, table.cellpadding, table.cellspacing, table.style, table.width, td.abbr, td.align, td.class, td.colspan, td.rowspan, td.style, td.valign, tr.align, tr.class, tr.style, tr.valign, th.abbr, th.align, th.class, th.colspan, th.rowspan, th.style, th.valign';
+		$HTML_Allowed_Attr = 'a.href, a.title, a.target, a.rel, abbr.title, acronym.title, blockquote.cite, div.align, div.style, div.class, font.size, font.color,	h1.style, h2.style, h3.style, h4.style, h5.style, h6.style, img.src, img.alt, img.title, img.class, img.align, img.style, p.style, span.style, span.class, table.class, table.border, table.cellpadding, table.cellspacing, table.style, table.width, td.abbr, td.align, td.class, td.colspan, td.rowspan, td.style, td.valign, tr.align, tr.class, tr.style, tr.valign, th.abbr, th.align, th.class, th.colspan, th.rowspan, th.style, th.valign';
 
 		// sets default config settings for htmpurifier
 		$icms_PurifyConfig = HTMLPurifier_Config::createDefault();
