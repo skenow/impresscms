157fbeccc4ee026b255514634b04bfa4d3ebd291
Sina Asghari <stranger@impresscms.org>
2008-08-31 08:23:53 +0000

    updating the JSCalendar library.

--- htdocs/class/xoopsform/formtextdateselect.php
+++ htdocs/class/xoopsform/formtextdateselect.php
@@ -69,11 +69,28 @@ class XoopsFormTextDateSelect extends XoopsFormText
 	 */
 	function render()
 	{
-    	$ele_name = $this->getName();
+	global $xoopsConfig;
+	if(!defined('_USE_PERSIANNUM') && defined('_EXT_DATE_FUNC') && $xoopsConfig['use_ext_date'] == 1 && _EXT_DATE_FUNC)
+	{
+	// For instance, we use different numbers in Persian, but the input must be in latin alphabets, thus we set the value off through here. I guess it must become a global value ...
+	define("_USE_PERSIANNUM","0");
+	}
+	   	$ele_name = $this->getName();
 		$ele_value = $this->getValue(false);
-		$jstime = formatTimestamp( $ele_value, 'F j Y, H:i:s' );
+		$jstime = formatTimestamp( $ele_value, 'Y-m-d' );
 		include_once XOOPS_ROOT_PATH.'/include/calendarjs.php';
-		return "<input type='text' name='".$ele_name."' id='".$ele_name."' size='".$this->getSize()."' maxlength='".$this->getMaxlength()."' value='".date("Y-m-d", $ele_value)."'".$this->getExtra()." /><input type='reset' value=' ... ' onclick='return showCalendar(\"".$ele_name."\");'>";
+//		return "<input type='text' name='".$ele_name."' id='".$ele_name."' size='".$this->getSize()."' maxlength='".$this->getMaxlength()."' value='".date("Y-m-d", $ele_value)."'".$this->getExtra()." /><input type='reset' value=' ... ' onclick='return showCalendar(\"".$ele_name."\");'>";
+// Now it is time to let users use their own calendars.
+		return "<input type='text' name='".$ele_name."' id='".$ele_name."' size='".$this->getSize()."' maxlength='".$this->getMaxlength()."' value='".date("Y-m-d", $ele_value)."' ".$this->getExtra()." onclick='return showCalendar(\"".$ele_name."\");' /><script type='text/javascript'>
+				Calendar.setup({
+					inputField  : '".$ele_name."',   // id of the input field
+					button      : '".$ele_name."',   // trigger for the calendar (button ID)
+		       		ifFormat    : '%Y-%m-%d',       // format of the input field
+        			langNumbers : true,
+        			dateType	: '"._CALENDAR_TYPE."'
+
+				});
+			</script>";
 	}
 }
 
