d9162af2cc3dc446bd4b6c08ead6364737c3486b
Sina Asghari <stranger@impresscms.org>
2008-09-02 21:11:05 +0000

    allowing HTML for banners & content manager (when it is sanitized by stopXSS it allows HTML)

--- htdocs/modules/system/admin/banners/main.php
+++ htdocs/modules/system/admin/banners/main.php
@@ -29,8 +29,8 @@ if(!is_object($xoopsUser) || !is_object($xoopsModule) || !$xoopsUser->isAdmin($x
 include_once ICMS_ROOT_PATH.'/modules/system/admin/banners/banners.php';
 include_once ICMS_ROOT_PATH.'/class/module.textsanitizer.php';
 
-if(!empty($_POST)) foreach($_POST as $k => $v) ${$k} = StopXSS($v);
-if(!empty($_GET)) foreach($_GET as $k => $v) ${$k} = StopXSS($v);
+	if(!empty($_POST)){ foreach($_POST as $k => $v){ if (!in_array($k,$allowedHTML)){${$k} = StopXSS($v);}else{${$k} = $v;}}}
+	if(!empty($_GET)){ foreach($_GET as $k => $v){ if (!in_array($k,$allowedHTML)){${$k} = StopXSS($v);}else{${$k} = $v;}}}
 $op = (isset($_GET['op']))?trim(StopXSS($_GET['op'])):((isset($_POST['op']))?trim(StopXSS($_POST['op'])):'BannersAdmin');
 switch($op)
 {
