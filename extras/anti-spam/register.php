f6fbe37e2dc02b8873c4c4680277bcf26369a14c
Sina Asghari <stranger@impresscms.org>
2008-10-25 14:44:01 +0000

    @marcan: following my last commit, it's better we add that file to our kernel, and add this file just as extras, nobody is using it, so ...

--- extras/anti-spam/register.php
+++ extras/anti-spam/register.php
@@ -13,83 +13,6 @@
 */
 
 /**
-* IcmsStopSpammer object
-*
-* This class is responsible for cross referencing register information with StopForumSpam.com API
-*
-* @copyright	The ImpressCMS Project http://www.impresscms.org/
-* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
-* @package		IcmsPersistableObject
-* @since		1.2
-* @version		$Id
-*/
-class IcmsStopSpammer {
-	private $api_url;
-
-	/**
-	 * Constructor
-	 */
-	public function __construct() {
-		// checkin stopforumspam API
-		$this->api_url = "http://www.stopforumspam.com/api?";
-	}
-
-	/**
-	 * Check the StopForumSpam API for a specific field (username, email or IP)
-	 *
-	 * @param string $field field to check
-	 * @param string $value value to validate
-	 * @return true if spammer was found with passed info
-	 */
-	function checkForField($field, $value) {
-		$spam = false;
-		$file = @fopen($this->api_url . $field . '=' . $value, "r");
-		if (!$file) {
-			echo "<script> alert('" . _US_SERVER_PROBLEM_OCCURRED . "'); window.history.go(-1); </script>\n";
-		}
-		while (!feof($file)) {
-			$line = fgets($file, 1024);
-			if (eregi("<appears>(.*)</appears>", $line, $out)) {
-				$spam = $out[1];
-				break;
-			}
-		}
-		fclose($file);
-		return $spam == 'yes';
-	}
-
-	/**
-	 * Check the StopForumSpam API for specified username
-	 *
-	 * @param string $username username to check
-	 * @return true if spammer was found with this username
-	 */
-	function badUsername($username) {
-		$ret = $this->checkForField('username', $username);
-	}
-
-	/**
-	 * Check the StopForumSpam API for specified email
-	 *
-	 * @param string $email email to check
-	 * @return true if spammer was found with this email
-	 */
-	function badEmail($email) {
-		$ret = $this->checkForField('email', $email);
-	}
-
-	/**
-	 * Check the StopForumSpam API for specified IP
-	 *
-	 * @param string $ip ip to check
-	 * @return true if spammer was found with this IP
-	 */
-	function badIP($ip) {
-		$ret = $this->checkForField('ip', $ip);
-	}
-}
-
-/**
  *  Registration process for new users
  *  Gathers required information and validates the new user
  *  @package kernel
@@ -112,6 +35,7 @@ if ($xoopsConfigUser['allow_register'] == 0 && $xoopsConfigUser['activation_type
 if ($_SESSION['xoopsUserId']) {
 	redirect_header('index.php', 6, _US_ALREADY_LOGED_IN);
 }
+include_once ICMS_ROOT_PATH . "/kernel/icmsstopspammer.php";
 /**
  *  Validates username, email address and password entries during registration
  *  Username is validated for uniqueness and length, password is validated for length and strictness,
