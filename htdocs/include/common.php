6fecb4b8a9ee3fa9f05b3ae4a4ed314840cd26e4
Vaughan Montgomery <vaughan@impresscms.org>
2008-12-05 21:26:18 +0000

    changing session.
    instead of regenerating session on every page (which is causing issues with admin for some users). we regenerate the id upon successful user login. the id will stay constant throughout the logged in session.  if a user is admin, and they goto admin section then the session id is again regenerated and the old one destroyed, but the session id will then be current for the session. if an admin returns to the userside, a new session id will again be regenerated..

--- htdocs/include/common.php
+++ htdocs/include/common.php
@@ -248,7 +248,6 @@ if ($xoopsConfig['use_ssl'] && isset($_POST[$xoopsConfig['sslpost_name']]) && $_
 }
 session_set_save_handler(array(&$sess_handler, 'open'), array(&$sess_handler, 'close'), array(&$sess_handler, 'read'), array(&$sess_handler, 'write'), array(&$sess_handler, 'destroy'), array(&$sess_handler, 'gc'));
 session_start();
-session_regenerate_id(true); // generate a new session identifier
 
 // Remove expired session for xoopsUserId
 if ( $xoopsConfig['use_mysession'] && $xoopsConfig['session_name'] != '' && !isset($_COOKIE[$xoopsConfig['session_name']]) && !empty($_SESSION['xoopsUserId']) ) {
@@ -333,7 +332,7 @@ if (!empty($_SESSION['xoopsUserId'])) {
     if (!is_object($xoopsUser)) {
     	$xoopsUser = '';
 		// Regenrate a new session id and destroy old session
-		session_regenerate_id(true);
+		$sess_handler->icms_sessionRegenerateId(true);
         $_SESSION = array();
 	} else {
     	if ($xoopsConfig['use_mysession'] && $xoopsConfig['session_name'] != '') {
