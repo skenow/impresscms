0cea19d077d1aac9aa30ccb8cf8aea0ceb49bfd7
Vaughan Montgomery <vaughan@impresscms.org>
2009-01-06 19:31:08 +0000

    bugfix, undefined index xoopsUserId in register.php

--- htdocs/register.php
+++ htdocs/register.php
@@ -31,7 +31,7 @@ $xoopsConfigUser =& $config_handler->getConfigsByCat(XOOPS_CONF_USER);
 if ($xoopsConfigUser['allow_register'] == 0 && $xoopsConfigUser['activation_type'] != 3) {
 	redirect_header('index.php', 6, _US_NOREGISTER);
 }
-if($_SESSION['xoopsUserId']){
+if(!empty($_SESSION['xoopsUserId']) && $_SESSION['xoopsUserId']) {
 	redirect_header('index.php', 6, _US_ALREADY_LOGED_IN);
 }
 /**
