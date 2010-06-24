941ee9ca2e9a29849dc62d38f259949da9b93e65
Steve Kenow <skenow@impresscms.org>
2010-06-24 03:25:51 +0000

    Providing legacy support (for 1 more version) for IcmsKernel; applying code standards to the icms_core_Kernel class

--- htdocs/libraries/icms/core/Kernel.php
+++ htdocs/libraries/icms/core/Kernel.php
@@ -14,24 +14,26 @@
  * Few notes:
  * - modules should use this class methods to generate physical paths/URIs (the ones which do not conform
  * will perform badly when true URL rewriting is implemented)
- * @package		kernel
+ *
+ * @category	ICMS
+ * @package		core
  * @since 		1.1
  */
-class icms_core_Kernel {
+class icms_core_Kernel extends icms_core_Object {
 	/** @var array */
-	var $paths = array(
+	public $paths = array(
 		'www' => array(), 'modules' => array(), 'themes' => array(),
 	);
 	/** @var array */
-	var $urls=false;
+	public $urls=false;
 
 	/**
 	 * Constructor for icms_core_Kernel, initiating all properties of the class
 	 */
-	function icms_core_Kernel() {
-		$this->paths['www'] = array( ICMS_ROOT_PATH, ICMS_URL );
-		$this->paths['modules'] = array( ICMS_ROOT_PATH . '/modules', ICMS_URL . '/modules' );
-		$this->paths['themes'] = array( ICMS_ROOT_PATH . '/themes', ICMS_URL . '/themes' );
+	public function __construct() {
+		$this->paths['www'] = array(ICMS_ROOT_PATH, ICMS_URL);
+		$this->paths['modules'] = array(ICMS_ROOT_PATH . '/modules', ICMS_URL . '/modules');
+		$this->paths['themes'] = array(ICMS_ROOT_PATH . '/themes', ICMS_URL . '/themes');
 		$this->_buildRelevantUrls();
 	}
 	/**
@@ -40,23 +42,24 @@ class icms_core_Kernel {
 	 * @param 	boolean	$virtual
 	 * @return 	string
 	 */
-	function path( $url, $virtual = false ) {
+	public function path($url, $virtual = false) {
 		$path = '';
-		@list( $root, $path ) = explode( '/', $url, 2 );
-		if ( !isset( $this->paths[$root] ) ) {
-			list( $root, $path ) = array( 'www', $url );
+		@list($root, $path) = explode('/', $url, 2);
+		if ( !isset($this->paths[$root]) ) {
+			list($root, $path) = array('www', $url);
 		}
-		if ( !$virtual ) {		// Returns a physical path
+		if ( !$virtual ) {
+			// Returns a physical path
 			return $this->paths[$root][0] . '/' . $path;
 		}
-		return !isset( $this->paths[$root][1] ) ? '' : ( $this->paths[$root][1] . '/' . $path );
+		return !isset($this->paths[$root][1]) ? '' : ( $this->paths[$root][1] . '/' . $path );
 	}
 	/**
 	 * Convert a ImpressCMS path to an URL
 	 * @param 	string	$url
 	 * @return 	string
 	 */
-	function url( $url ) {
+	public function url($url) {
 		return ( false !== strpos( $url, '://' ) ? $url : $this->path( $url, true ) );
 	}
 	/**
@@ -65,21 +68,21 @@ class icms_core_Kernel {
 	 * @param 	array	$params
 	 * @return 	string
 	 */
-	function buildUrl( $url, $params = array() ) {
+	public function buildUrl($url, $params = array()) {
 		if ( $url == '.' ) {
 			$url = $_SERVER['REQUEST_URI'];
 		}
-		$split = explode( '?', $url );
+		$split = explode('?', $url);
 		if ( count($split) > 1 ) {
-			list( $url, $query ) = $split;
-			parse_str( $query, $query );
-			$params = array_merge( $query, $params );
+			list($url, $query) = $split;
+			parse_str($query, $query);
+			$params = array_merge($query, $params);
 		}
-		if ( !empty( $params ) ) {
+		if ( !empty($params) ) {
 			foreach ( $params as $k => $v ) {
 				$params[$k] = $k . '=' . rawurlencode($v);
 			}
-			$url .= '?' . implode( '&', $params );
+			$url .= '?' . implode('&', $params);
 		}
 		return $url;
 	}
@@ -88,7 +91,7 @@ class icms_core_Kernel {
 	 * Build URLs for global use throughout the application
 	 * @return 	array
 	 */
-	function _buildRelevantUrls() {
+	public function _buildRelevantUrls() {
 
 		if (!$this->urls) {
 			$http = ((strpos(ICMS_URL, "https://")) === false) ? ("http://") : ("https://");
@@ -106,17 +109,13 @@ class icms_core_Kernel {
 			$this->urls['querystring'] = $querystring;
 			$this->urls['full_phpself'] = $http . $httphost . $phpself;
 			$this->urls['full'] = $currenturl;
-				
+
 			$previouspage = '';
-			if ( array_key_exists( 'HTTP_REFERER', $_SERVER) && isset($_SERVER['HTTP_REFERER']) ) {
+			if ( array_key_exists('HTTP_REFERER', $_SERVER) && isset($_SERVER['HTTP_REFERER']) ) {
 				$this->urls['previouspage'] = $_SERVER['HTTP_REFERER'];
 			}
 			//$this->urls['isHomePage'] = (ICMS_URL . "/index.php") == ($http . $httphost . $phpself);
 		}
 		return $this->urls;
 	}
-
-
 }
-
-?>
