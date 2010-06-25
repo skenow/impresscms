a5186bae9929445ab203d51798d0121147de6ff1
Steve Kenow <skenow@impresscms.org>
2010-06-25 15:24:58 +0000

    Further work on the Textsanitizer class, replacing deprecated methods throughout the core

--- htdocs/include/functions.php
+++ htdocs/include/functions.php
@@ -713,7 +713,7 @@ function xoops_getrank($rank_id =0, $posts = 0)
 		$sql = "SELECT rank_title AS title, rank_image AS image FROM ".$db->prefix('ranks')." WHERE rank_min <= '".$posts."' AND rank_max >= '".$posts."' AND rank_special = '0'";
 	}
 	$rank = $db->fetchArray($db->query($sql));
-	$rank['title'] = $myts->makeTboxData4Show($rank['title']);
+	$rank['title'] = $myts->htmlSpecialChars($rank['title']);
 	$rank['id'] = $rank_id;
 	return $rank;
 }
