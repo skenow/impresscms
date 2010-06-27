a2ae781a8db7352fac744219ace306141106c522
Marc-André Lanciault <marcan@impresscms.org>
2010-06-27 11:38:31 +0000

    calling __construct instead of getInstance...

--- htdocs/kernel/avatar.php
+++ htdocs/kernel/avatar.php
@@ -4,7 +4,7 @@ defined('ICMS_ROOT_PATH') or die('ImpressCMS root path not defined');
 class XoopsAvatar extends icms_avatar_Object {
 	private $_deprecated;
 	public function __construct() {
-		parent::getInstance();
+		parent::__construct();
 		$this->_deprecated = icms_deprecated('icms_avatar_Object', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
@@ -12,7 +12,7 @@ class XoopsAvatar extends icms_avatar_Object {
 class XoopsAvatarHandler extends icms_avatar_Handler {
 	private $_deprecated;
 	public function __construct() {
-		parent::getInstance();
+		parent::__construct();
 		$this->_deprecated = icms_deprecated('icms_avatar_Handler', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
