1a0d31fb28917e22f50258eb28b065504c5aab65
Marc-André Lanciault <marcan@impresscms.org>
2010-06-27 11:17:39 +0000

    adding deprecated message in kernel/avatar.php

--- htdocs/kernel/avatar.php
+++ htdocs/kernel/avatar.php
@@ -17,11 +17,19 @@
 if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
 
 class XoopsAvatar extends icms_avatar_Object {
-
+	private $_deprecated;
+	public function __construct() {
+		parent::getInstance();
+		$this->_deprecated = icms_deprecated('icms_avatar_Object', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
+	}
 }
 
 class XoopsAvatarHandler extends icms_avatar_Handler {
-
+	private $_deprecated;
+	public function __construct() {
+		parent::getInstance();
+		$this->_deprecated = icms_deprecated('icms_avatar_Object', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
+	}
 }
 
 ?>
