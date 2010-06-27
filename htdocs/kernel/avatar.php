b292cc1c2e467dfee4740df27b09f805b71df3cb
Marc-André Lanciault <marcan@impresscms.org>
2010-06-27 11:25:42 +0000

    cleaning up deprecated kernel/avatar.php

--- htdocs/kernel/avatar.php
+++ htdocs/kernel/avatar.php
@@ -1,19 +1,4 @@
 <?php
-/**
- * Manage avatars for users
- *
- * @copyright	http://www.xoops.org/ The XOOPS Project
- * @copyright	XOOPS_copyrights.txt
- * @copyright	http://www.impresscms.org/ The ImpressCMS Project
- * @license		LICENSE.txt
- * @package		core
- * @subpackage	avatar
- * @since		XOOPS
- * @author		Kazumi Ono (aka onokazo)
- * @author		http://www.xoops.org The XOOPS Project
- * @version		$Id$
- */
-
 if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
 
 class XoopsAvatar extends icms_avatar_Object {
@@ -31,5 +16,3 @@ class XoopsAvatarHandler extends icms_avatar_Handler {
 		$this->_deprecated = icms_deprecated('icms_avatar_Object', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
-
-?>
