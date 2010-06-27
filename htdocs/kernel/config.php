d255e9c26c4395b8c0521b886bd68eeb46ba11a0
Marc-André Lanciault <marcan@impresscms.org>
2010-06-27 11:37:21 +0000

    cleaning up deprecated kernel/config.php

--- htdocs/kernel/config.php
+++ htdocs/kernel/config.php
@@ -1,33 +1,6 @@
 <?php
-/**
- * Manage configuration items
- *
- * @copyright	http://www.xoops.org/ The XOOPS Project
- * @copyright	XOOPS_copyrights.txt
- * @copyright	http://www.impresscms.org/ The ImpressCMS Project
- * @license		LICENSE.txt
- * @package		core
- * @subpackage	config
- * @since		XOOPS
- * @author		Kazumi Ono (aka onokazo)
- * @author		http://www.xoops.org The XOOPS Project
- * @version		$Id: config.php 19431 2010-06-16 20:46:34Z david-sf $
- */
+defined('ICMS_ROOT_PATH') or die('ImpressCMS root path not defined');
 
-/**
- * XOOPS configuration handling class.
- * This class acts as an interface for handling general configurations of XOOPS
- * and its modules.
- *
- * @package 	kernel
- * @subpackage 	config
- * @author  Kazumi Ono <webmaster@myweb.ne.jp>
- * @todo    Tests that need to be made:
- *          - error handling
- * @access  public
- * @deprecated	Use icms_config_Handler, instead
- * @todo		Remove in version 1.4
- */
 class XoopsConfigHandler extends icms_config_Handler {
 	private $_deprecated;
 	public function __construct() {
