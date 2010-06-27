0526855351bfab6af366b695068fe7e56b6fbe23
Marc-André Lanciault <marcan@impresscms.org>
2010-06-27 11:27:20 +0000

    typo

--- htdocs/kernel/avatar.php
+++ htdocs/kernel/avatar.php
@@ -13,6 +13,6 @@ class XoopsAvatarHandler extends icms_avatar_Handler {
 	private $_deprecated;
 	public function __construct() {
 		parent::getInstance();
-		$this->_deprecated = icms_deprecated('icms_avatar_Object', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
+		$this->_deprecated = icms_deprecated('icms_avatar_Handler', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
