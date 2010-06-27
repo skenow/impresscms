527854cbfec0a26ec95f059e7dd012b5d42018fe
Marc-André Lanciault <marcan@impresscms.org>
2010-06-27 11:27:33 +0000

    cleaning up deprecated kernel/block.php

--- htdocs/kernel/block.php
+++ htdocs/kernel/block.php
@@ -1,40 +1,18 @@
 <?php
-/**
- * XoopsBlock - for backwards compatibility
- *
- * @since XOOPS
- * @copyright The XOOPS Project <http://www.xoops.org>
- * @author The XOOPS Project Community <http://www.xoops.org>
- *
- * @see IcmsBlock
- *
- * @deprecated use icms_core_Block instead
- * @todo Remove in version 1.4 - all instances have been removed from the core
- */
-class XoopsBlock extends icms_core_Block {
-	public function __construct(&$db) {
-		parent::__construct(&$db);
-		$this->setErrors = icms_deprecated('icms_core_Block');
-	}
+if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
 
+class XoopsBlock extends icms_core_Block {
+	private $_deprecated;
+	public function __construct() {
+		parent::getInstance();
+		$this->_deprecated = icms_deprecated('icms_core_Block', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
+	}
 }
 
-/**
- * XoopsBlockHandler - For backwards compatibility
- *
- * @since XOOPS
- * @copyright The XOOPS Project <http://www.xoops.org>
- * @author The XOOPS Project Community <http://www.xoops.org>
- *
- * @see IcmsBlockHandler
- *
- * @deprecated  use icms_core_BlockHandler instead
- * @todo Remove in version 1.4 - all instances have been removed from the core
- */
 class XoopsBlockHandler extends icms_core_BlockHandler {
-	public function __construct(&$db) {
-		parent::__construct(&$db);
-		$this->setVar('_errors', icms_deprecated('icms_core_BlockHandler'));
+	private $_deprecated;
+	public function __construct() {
+		parent::getInstance();
+		$this->_deprecated = icms_deprecated('icms_core_BlockHandler', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
-
 }
