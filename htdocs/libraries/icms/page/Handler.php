e369803f861161eba2c6643849f7770b7f45a80b
Steve Kenow <skenow@impresscms.org>
2010-06-30 03:56:20 +0000

    Completing conversion of Page - old class deprecated and new class formatted to follow code standards (modified Zend)

--- htdocs/libraries/icms/page/Handler.php
+++ htdocs/libraries/icms/page/Handler.php
@@ -3,12 +3,13 @@
  * Classes responsible for managing core page objects
  *
  * @copyright	The ImpressCMS Project <http://www.impresscms.org/>
- * @license	LICENSE.txt
- * @package	core
- * @since	ImpressCMS 1.1
- * @author	modified by UnderDog <underdog@impresscms.org>
- * @author	Gustavo Pilla (aka nekro) <nekro@impresscms.org> <gpilla@nubee.com.ar>
- * @version	$Id: page.php 19450 2010-06-18 14:15:29Z malanciault $
+ * @license		LICENSE.txt
+ * @category	ICMS
+ * @package		Page
+ * @since		ImpressCMS 1.1
+ * @author		modified by UnderDog <underdog@impresscms.org>
+ * @author		Gustavo Pilla (aka nekro) <nekro@impresscms.org> <gpilla@nubee.com.ar>
+ * @version		SVN: $Id$
  */
 
 defined('ICMS_ROOT_PATH') or die('ImpressCMS root path not defined');
@@ -22,21 +23,21 @@ defined('ICMS_ROOT_PATH') or die('ImpressCMS root path not defined');
 class icms_page_Handler extends icms_ipf_Handler {
 
 	public function __construct( & $db ){
-		parent::__construct($db, 'page' ,'page_id' ,'page_title', '' , 'icms');
+		parent::__construct($db, 'page', 'page_id', 'page_title', '', 'icms');
 		$this->table = $db->prefix('icmspage');
 	}
 
-	public function getList( $criteria = null, $limit = 0, $start = 0, $debug=false){
+	public function getList($criteria = null, $limit = 0, $start = 0, $debug=false) {
 		$rtn = array();
-		$pages =& $this->getObjects( $criteria, true );
+		$pages =& $this->getObjects($criteria, true);
 		foreach( $pages as $page ) {
-			$rtn[$page->getVar('page_moduleid').'-'.$page->getVar('page_id')] = $page->getVar('page_title');
+			$rtn[$page->getVar('page_moduleid') . '-' . $page->getVar('page_id')] = $page->getVar('page_title');
 		}
 		return $rtn;
 	}
 
-	public function getPageSelOptions($value=null){
-		if (!is_array($value)){
+	public function getPageSelOptions($value=null) {
+		if ( !is_array($value) ){
 			$value = array($value);
 		}
 		$module_handler =& xoops_gethandler('module');
@@ -44,22 +45,25 @@ class icms_page_Handler extends icms_ipf_Handler {
 		$criteria->add(new icms_criteria_Item('isactive', 1));
 		$module_list =& $module_handler->getObjects($criteria);
 		$mods = '';
-		foreach ($module_list as $module){
-			$mods .= '<optgroup label="'.$module->getVar('name').'">';
+		foreach ( $module_list as $module ) {
+			$mods .= '<optgroup label="' . $module->getVar('name') . '">';
 			$criteria = new icms_criteria_Compo(new icms_criteria_Item('page_moduleid', $module->getVar('mid')));
 			$criteria->add(new icms_criteria_Item('page_status', 1));
 			$pages =& $this->getObjects($criteria);
 			$sel = '';
-			if (in_array($module->getVar('mid').'-0',$value)){
+			if ( in_array($module->getVar('mid') . '-0', $value) ) {
 				$sel = ' selected=selected';
 			}
-			$mods .= '<option value="'.$module->getVar('mid').'-0"'.$sel.'>'._AM_ALLPAGES.'</option>';
-			foreach ($pages as $page){
+			$mods .= '<option value="' . $module->getVar('mid') . '-0"' . $sel . '>' . _AM_ALLPAGES . '</option>';
+			foreach ( $pages as $page ) {
 				$sel = '';
-				if (in_array($module->getVar('mid').'-'.$page->getVar('page_id'),$value)){
+				if ( in_array($module->getVar('mid') . '-' . $page->getVar('page_id'), $value) ) {
 					$sel = ' selected=selected';
 				}
-				$mods .= '<option value="'.$module->getVar('mid').'-'.$page->getVar('page_id').'"'.$sel.'>'.$page->getVar('page_title').'</option>';
+				$mods .= '<option value="'
+						. $module->getVar('mid') . '-' . $page->getVar('page_id') . '"' . $sel . '>'
+						. $page->getVar('page_title')
+						.'</option>';
 			}
 			$mods .= '</optgroup>';
 		}
@@ -69,33 +73,37 @@ class icms_page_Handler extends icms_ipf_Handler {
 		$criteria->add(new icms_criteria_Item('page_status', 1));
 		$pages =& $this->getObjects($criteria);
 		$cont = '';
-		if (count($pages) > 0){
-			$cont = '<optgroup label="'.$module->getVar('name').'">';
+		if ( count($pages) > 0 ){
+			$cont = '<optgroup label="' . $module->getVar('name') . '">';
 			$sel = '';
-			if (in_array($module->getVar('mid').'-0',$value)){
+			if ( in_array($module->getVar('mid').'-0', $value) ) {
 				$sel = ' selected=selected';
 			}
-			$cont .= '<option value="'.$module->getVar('mid').'-0"'.$sel.'>'._AM_ALLPAGES.'</option>';
-			foreach ($pages as $page){
+			$cont .= '<option value="' . $module->getVar('mid') . '-0"' . $sel . '>' . _AM_ALLPAGES . '</option>';
+			foreach ( $pages as $page ) {
 				$sel = '';
-				if (in_array($module->getVar('mid').'-'.$page->getVar('page_id'),$value)){
+				if ( in_array($module->getVar('mid') . '-' . $page->getVar('page_id'), $value) ) {
 					$sel = ' selected=selected';
 				}
-				$cont .= '<option value="'.$module->getVar('mid').'-'.$page->getVar('page_id').'"'.$sel.'>'.$page->getVar('page_title').'</option>';
+				$cont .= '<option value="' . $module->getVar('mid') . '-' . $page->getVar('page_id') . '"' . $sel . '>'
+						. $page->getVar('page_title')
+						. '</option>';
 			}
 			$cont .= '</optgroup>';
 		}
 		$sel = $sel1 = '';
-		if (in_array('0-1',$value)){
+		if ( in_array('0-1',$value) ) {
 			$sel = ' selected=selected';
 		}
-		if (in_array('0-0',$value)){
+		if ( in_array('0-0',$value) ) {
 			$sel1 = ' selected=selected';
 		}
-		$ret = '<option value="0-1"'.$sel.'>'._AM_TOPPAGE.'</option><option value="0-0"'.$sel1.'>'._AM_ALLPAGES.'</option>';
-		$ret .= $cont.$mods;
+		$ret = '<option value="0-1"' . $sel . '>'. _AM_TOPPAGE
+			. '</option><option value="0-0"' . $sel1 . '>' . _AM_ALLPAGES
+			. '</option>';
+		$ret .= $cont . $mods;
 
 		return $ret;
 	}
 }
-?>
+
--- htdocs/libraries/icms/page/Object.php
+++ htdocs/libraries/icms/page/Object.php
@@ -3,12 +3,13 @@
  * Classes responsible for managing core page objects
  *
  * @copyright	The ImpressCMS Project <http://www.impresscms.org/>
- * @license	LICENSE.txt
- * @package	core
- * @since	ImpressCMS 1.1
- * @author	modified by UnderDog <underdog@impresscms.org>
- * @author	Gustavo Pilla (aka nekro) <nekro@impresscms.org> <gpilla@nubee.com.ar>
- * @version	$Id: page.php 19450 2010-06-18 14:15:29Z malanciault $
+ * @license		LICENSE.txt
+ * @category	ICMS
+ * @package		Page
+ * @since		ImpressCMS 1.1
+ * @author		modified by UnderDog <underdog@impresscms.org>
+ * @author		Gustavo Pilla (aka nekro) <nekro@impresscms.org> <gpilla@nubee.com.ar>
+ * @version		SVN: $Id$
  */
 
 defined('ICMS_ROOT_PATH') or die('ImpressCMS root path not defined');
@@ -23,7 +24,7 @@ class icms_page_Object extends icms_ipf_Object {
 
 	public function __construct( & $handler ){
 
-		$this->icms_ipf_Object( $handler );
+		parent::__construct( $handler );
 
 		$this->quickInitVar('page_id', XOBJ_DTYPE_INT);
         $this->quickInitVar('page_moduleid', XOBJ_DTYPE_INT, true);
@@ -32,4 +33,4 @@ class icms_page_Object extends icms_ipf_Object {
         $this->quickInitVar('page_status', XOBJ_DTYPE_INT, true);
 	}
 }
-?>
+
