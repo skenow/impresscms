f18891209881513cd46c5efcc295092a5df34ffb
Steve Kenow <skenow@impresscms.org>
2010-06-30 16:03:40 +0000

    Completing conversion of Template Set classes - old classes deprecated and new class formatted to follow code standards (modified Zend)

--- htdocs/libraries/icms/view/template/set/Handler.php
+++ htdocs/libraries/icms/view/template/set/Handler.php
@@ -1,30 +1,29 @@
 <?php
 /**
- * Manage of template sets
+ * Manage template sets
  *
- * @copyright	http://www.xoops.org/ The XOOPS Project
- * @copyright	XOOPS_copyrights.txt
  * @copyright	http://www.impresscms.org/ The ImpressCMS Project
- * @license	LICENSE.txt
- * @package	core
- * @since	XOOPS
- * @author	http://www.xoops.org The XOOPS Project
- * @author	modified by UnderDog <underdog@impresscms.org>
- * @version	$Id: tplset.php 19459 2010-06-18 14:58:18Z malanciault $
+ * @license		LICENSE.txt
+ * @category	ICMS
+ * @package		View
+ * @subpackage	Template
+ * @version		SVN: $Id$
  */
 
-if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
+defined('ICMS_ROOT_PATH') or die("ImpressCMS root path not defined");
 
 /**
- * XOOPS tplset handler class.
+ * Template set handler class.
  * This class is responsible for providing data access mechanisms to the data source
- * of XOOPS tplset class objects.
+ * of template set class objects.
  *
  *
- * @author  Kazumi Ono <onokazu@xoops.org>
+ * @author		Kazumi Ono <onokazu@xoops.org>
+ * @category	ICMS
+ * @package		View
+ * @subpackage	Template
  */
-class icms_view_template_set_Handler extends icms_core_ObjectHandler
-{
+class icms_view_template_set_Handler extends icms_core_ObjectHandler {
 
 	/**
 	 * create a new templateset instance
@@ -33,10 +32,9 @@ class icms_view_template_set_Handler extends icms_core_ObjectHandler
 	 * @param bool $isNew is the new tempateset new??
 	 * @return object icms_view_template_set_Object {@link icms_view_template_set_Object} reference to the new template
 	 **/
-	function &create($isNew = true)
-	{
+	public function &create($isNew = true) {
 		$tplset = new icms_view_template_set_Object();
-		if ($isNew) {
+		if ( $isNew ) {
 			$tplset->setNew();
 		}
 		return $tplset;
@@ -49,17 +47,17 @@ class icms_view_template_set_Handler extends icms_core_ObjectHandler
 	 * @param int $id of the tempateset to get
 	 * @return object icms_view_template_set_Object {@link icms_view_template_set_Object} reference to the new template
 	 **/
-	function &get($id)
-	{
+	public function &get($id) {
 		$tplset = false;
-		$id = (int) ($id);
-		if ($id > 0) {
-			$sql = "SELECT * FROM ".$this->db->prefix('tplset')." WHERE tplset_id='".$id."'";
-			if (!$result = $this->db->query($sql)) {
+		$id = (int) $id;
+		if ( $id > 0 ) {
+			$sql = "SELECT * FROM ".$this->db->prefix('tplset')
+				. " WHERE tplset_id='" . $id . "'";
+			if ( !$result = $this->db->query($sql) ) {
 				return $tplset;
 			}
 			$numrows = $this->db->getRowsNum($result);
-			if ($numrows == 1) {
+			if ( $numrows == 1 ) {
 				$tplset = new icms_view_template_set_Object();
 				$tplset->assignVars($this->db->fetchArray($result));
 			}
@@ -74,17 +72,17 @@ class icms_view_template_set_Handler extends icms_core_ObjectHandler
 	 * @param string $tplset_name of the tempateset to get
 	 * @return object icms_view_template_set_Object {@link icms_view_template_set_Object} reference to the new template
 	 **/
-	function &getByName($tplset_name)
-	{
+	public function &getByName($tplset_name) {
 		$tplset = false;
 		$tplset_name = trim($tplset_name);
-		if ($tplset_name != '') {
-			$sql = "SELECT * FROM ".$this->db->prefix('tplset')." WHERE tplset_name=".$this->db->quoteString($tplset_name)."";
-			if (!$result = $this->db->query($sql)) {
+		if ( $tplset_name != '' ) {
+			$sql = "SELECT * FROM " . $this->db->prefix('tplset')
+				. " WHERE tplset_name=" . $this->db->quoteString($tplset_name) . "";
+			if ( !$result = $this->db->query($sql) ) {
 				return $tplset;
 			}
 			$numrows = $this->db->getRowsNum($result);
-			if ($numrows == 1) {
+			if ( $numrows == 1 ) {
 				$tplset = new icms_view_template_set_Object();
 				$tplset->assignVars($this->db->fetchArray($result));
 			}
@@ -99,33 +97,52 @@ class icms_view_template_set_Handler extends icms_core_ObjectHandler
 	 * @param string $tplset_name of the tempateset to get
 	 * @return object icms_view_template_set_Object {@link icms_view_template_set_Object} reference to the new template
 	 **/
-	function insert(&$tplset)
-	{
-		/**
-		 * @TODO: Change to if (!(class_exists($this->className) && $obj instanceof $this->className)) when going fully PHP5
-		 */
-		if (!is_a($tplset, 'xoopstplset')) {
+	public function insert(&$tplset) {
+		/* As of PHP5.3.0, is_as() is no longer deprecated */
+		if ( !is_a($tplset, 'icms_view_template_set_Object') ) {
 			return false;
 		}
-		if (!$tplset->isDirty()) {
+		if ( !$tplset->isDirty() ) {
 			return true;
 		}
-		if (!$tplset->cleanVars()) {
+		if ( !$tplset->cleanVars() ) {
 			return false;
 		}
-		foreach ($tplset->cleanVars as $k => $v) {
+		foreach ( $tplset->cleanVars as $k => $v ) {
 			${$k} = $v;
 		}
-		if ($tplset->isNew()) {
+		if ( $tplset->isNew() ) {
 			$tplset_id = $this->db->genId('tplset_tplset_id_seq');
-			$sql = sprintf("INSERT INTO %s (tplset_id, tplset_name, tplset_desc, tplset_credits, tplset_created) VALUES ('%u', %s, %s, %s, '%u')", $this->db->prefix('tplset'), (int) ($tplset_id), $this->db->quoteString($tplset_name), $this->db->quoteString($tplset_desc), $this->db->quoteString($tplset_credits), (int) ($tplset_created));
+			$sql = sprintf(
+				"INSERT INTO %s (tplset_id, tplset_name, tplset_desc, tplset_credits, tplset_created)
+				VALUES ('%u', %s, %s, %s, '%u')",
+				$this->db->prefix('tplset'),
+				(int) $tplset_id,
+				$this->db->quoteString($tplset_name),
+				$this->db->quoteString($tplset_desc),
+				$this->db->quoteString($tplset_credits),
+				(int) $tplset_created
+			);
 		} else {
-			$sql = sprintf("UPDATE %s SET tplset_name = %s, tplset_desc = %s, tplset_credits = %s, tplset_created = '%u' WHERE tplset_id = '%u'", $this->db->prefix('tplset'), $this->db->quoteString($tplset_name), $this->db->quoteString($tplset_desc), $this->db->quoteString($tplset_credits), (int) ($tplset_created), (int) ($tplset_id));
+			$sql = sprintf(
+				"UPDATE %s SET
+				tplset_name = %s,
+				tplset_desc = %s,
+				tplset_credits = %s,
+				tplset_created = '%u'
+				WHERE tplset_id = '%u'",
+				$this->db->prefix('tplset'),
+				$this->db->quoteString($tplset_name),
+				$this->db->quoteString($tplset_desc),
+				$this->db->quoteString($tplset_credits),
+				(int) $tplset_created,
+				(int) $tplset_id
+			);
 		}
-		if (!$result = $this->db->query($sql)) {
+		if ( !$result = $this->db->query($sql) ) {
 			return false;
 		}
-		if (empty($tplset_id)) {
+		if ( empty($tplset_id) ) {
 			$tplset_id = $this->db->getInsertId();
 		}
 		$tplset->assignVar('tplset_id', $tplset_id);
@@ -139,20 +156,25 @@ class icms_view_template_set_Handler extends icms_core_ObjectHandler
 	 * @param object $tplset {@link icms_view_template_set_Object} reference to the object of the tempateset to delete
 	 * @return object icms_view_template_set_Object {@link icms_view_template_set_Object} reference to the new template
 	 **/
-	function delete(&$tplset)
-	{
-		/**
-		 * @TODO: Change to if (!(class_exists($this->className) && $obj instanceof $this->className)) when going fully PHP5
-		 */
-		if (!is_a($tplset, 'xoopstplset')) {
+	public function delete(&$tplset) {
+		/* As of PHP5.3.0, ia_a() is no longer deprecated */
+		if ( !is_a($tplset, 'icms_view_template_set_Object') ) {
 			return false;
 		}
 
-		$sql = sprintf("DELETE FROM %s WHERE tplset_id = '%u'", $this->db->prefix('tplset'), (int) ($tplset->getVar('tplset_id')));
-		if (!$result = $this->db->query($sql)) {
+		$sql = sprintf(
+			"DELETE FROM %s WHERE tplset_id = '%u'",
+			$this->db->prefix('tplset'),
+			(int) $tplset->getVar('tplset_id')
+		);
+		if ( !$result = $this->db->query($sql) ) {
 			return false;
 		}
-		$sql = sprintf("DELETE FROM %s WHERE tplset_name = %s", $this->db->prefix('imgset_tplset_link'), $this->db->quoteString($tplset->getVar('tplset_name')));
+		$sql = sprintf(
+			"DELETE FROM %s WHERE tplset_name = %s",
+			$this->db->prefix('imgset_tplset_link'),
+			$this->db->quoteString($tplset->getVar('tplset_name'))
+		);
 		$this->db->query($sql);
 		return true;
 	}
@@ -163,24 +185,23 @@ class icms_view_template_set_Handler extends icms_core_ObjectHandler
 	 * @param bool $id_as_key should the tplfile's tpl_id be the key for the returned array?
 	 * @return array {@link icms_view_template_set_Object}s matching the conditions
 	 **/
-	function getObjects($criteria = null, $id_as_key = false)
-	{
+	public function getObjects($criteria = null, $id_as_key = false) {
 		$ret = array();
 		$limit = $start = 0;
 		$sql = 'SELECT * FROM '.$this->db->prefix('tplset');
-		if (isset($criteria) && is_subclass_of($criteria, 'icms_criteria_Element')) {
-			$sql .= ' '.$criteria->renderWhere().' ORDER BY tplset_id';
+		if ( isset($criteria) && is_subclass_of($criteria, 'icms_criteria_Element') ) {
+			$sql .= ' ' . $criteria->renderWhere() . ' ORDER BY tplset_id';
 			$limit = $criteria->getLimit();
 			$start = $criteria->getStart();
 		}
 		$result = $this->db->query($sql, $limit, $start);
-		if (!$result) {
+		if ( !$result ) {
 			return $ret;
 		}
-		while ($myrow = $this->db->fetchArray($result)) {
+		while ( $myrow = $this->db->fetchArray($result) ) {
 			$tplset = new icms_view_template_set_Object();
 			$tplset->assignVars($myrow);
-			if (!$id_as_key) {
+			if ( !$id_as_key ) {
 				$ret[] =& $tplset;
 			} else {
 				$ret[$myrow['tplset_id']] =& $tplset;
@@ -196,13 +217,12 @@ class icms_view_template_set_Handler extends icms_core_ObjectHandler
 	 * @param   object  $criteria   {@link icms_criteria_Element}
 	 * @return  int $count number of template filesets that match the criteria
 	 **/
-	function getCount($criteria = null)
-	{
-		$sql = 'SELECT COUNT(*) FROM '.$this->db->prefix('tplset');
-		if (isset($criteria) && is_subclass_of($criteria, 'icms_criteria_Element')) {
-			$sql .= ' '.$criteria->renderWhere();
+	public function getCount($criteria = null) {
+		$sql = 'SELECT COUNT(*) FROM ' . $this->db->prefix('tplset');
+		if ( isset($criteria) && is_subclass_of($criteria, 'icms_criteria_Element') ) {
+			$sql .= ' ' . $criteria->renderWhere();
 		}
-		if (!$result =& $this->db->query($sql)) {
+		if ( !$result =& $this->db->query($sql) ) {
 			return 0;
 		}
 		list($count) = $this->db->fetchRow($result);
@@ -216,15 +236,13 @@ class icms_view_template_set_Handler extends icms_core_ObjectHandler
 		*
 		* @return array  The array of tplset objects
 		*/
-	function getList($criteria = null)
-	{
+	public function getList($criteria = null) {
 		$ret = array();
 		$tplsets = $this->getObjects($criteria, true);
-		foreach (array_keys($tplsets) as $i) {
+		foreach ( array_keys($tplsets) as $i ) {
 			$ret[$tplsets[$i]->getVar('tplset_name')] = $tplsets[$i]->getVar('tplset_name');
 		}
 		return $ret;
 	}
 }
 
-?>
--- htdocs/libraries/icms/view/template/set/Object.php
+++ htdocs/libraries/icms/view/template/set/Object.php
@@ -1,40 +1,31 @@
 <?php
 /**
- * Manage of template sets
+ * Manage template sets
  *
- * @copyright	http://www.xoops.org/ The XOOPS Project
- * @copyright	XOOPS_copyrights.txt
  * @copyright	http://www.impresscms.org/ The ImpressCMS Project
- * @license	LICENSE.txt
- * @package	core
- * @since	XOOPS
- * @author	http://www.xoops.org The XOOPS Project
- * @author	modified by UnderDog <underdog@impresscms.org>
- * @version	$Id: tplset.php 19459 2010-06-18 14:58:18Z malanciault $
+ * @license		LICENSE.txt
+ * @category	ICMS
+ * @package		View
+ * @subpackage	Template
+ * @version		SVN: $Id$
  */
 
-if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
+defined('ICMS_ROOT_PATH') or die("ImpressCMS root path not defined");
 
 /**
- * @package kernel
- * @copyright copyright &copy; 2000 XOOPS.org
- */
-
-/**
- * Base class for all templatesets
+ * Base class for all template sets
  *
- * @author Kazumi Ono (AKA onokazu)
- * @copyright copyright &copy; 2000 XOOPS.org
- * @package kernel
+ * @author		Kazumi Ono (AKA onokazu)
+ * @category	ICMS
+ * @package		View
+ * @subpackage	Template
  **/
-class icms_view_template_set_Object extends icms_core_Object
-{
+class icms_view_template_set_Object extends icms_core_Object {
 
 	/**
 	 * constructor
 	 */
-	function icms_view_template_set_Object()
-	{
+	public function __construct() {
 		parent::__construct();
 		$this->initVar('tplset_id', XOBJ_DTYPE_INT, null, false);
 		$this->initVar('tplset_name', XOBJ_DTYPE_OTHER, null, false);
@@ -43,4 +34,4 @@ class icms_view_template_set_Object extends icms_core_Object
 		$this->initVar('tplset_created', XOBJ_DTYPE_INT, 0, false);
 	}
 }
-?>
+
