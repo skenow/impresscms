eb06279da8f82408add21e19dd66477b07e8dd6b
Steve Kenow <skenow@impresscms.org>
2010-11-16 01:14:56 +0000

    Updating version and status info for the final release

--- htdocs/include/version.php
+++ htdocs/include/version.php
@@ -10,7 +10,7 @@
 * @version		$Id$
 */
 
-define('ICMS_VERSION_NAME','ImpressCMS 1.2.3 RC2');
+define('ICMS_VERSION_NAME','ImpressCMS 1.2.3 Final');
 /**
  * To developers:
  * if you want to get the version number of the core, please use something like:
@@ -31,15 +31,15 @@ define('XOOPS_VERSION', ICMS_VERSION_NAME);
  * 10 = Final
  */
 
-define('ICMS_VERSION_STATUS', 3);
+define('ICMS_VERSION_STATUS', 10);
 
 /**
  * Build number
  *
  * Every release has its own build number, incrementable by 1 everytime we make a release
  */
-// impresscms_1.2.3 RC = 38
-define('ICMS_VERSION_BUILD', 39);
+// impresscms_1.2.3 RC2 = 39
+define('ICMS_VERSION_BUILD', 40);
 
 /**
  * Latest dbversion of the System Module
