4cc59815557893d237c74b62bc5829b841c4e973
Marc-André Lanciault <marcan@impresscms.org>
2010-11-18 21:37:39 +0000

    formating

--- htdocs/class/database/drivers/pdo/database.php
+++ htdocs/class/database/drivers/pdo/database.php
@@ -4,7 +4,7 @@
 // JULIAN EGELSTAFF - NOV 16 2010
 
 if (!defined("ICMS_ROOT_PATH")) {
-    die("ImpressCMS root path not defined");
+	die("ImpressCMS root path not defined");
 }
 
 include_once ICMS_ROOT_PATH."/class/database/database.php";
@@ -18,7 +18,7 @@ class XoopsPDODatabase extends XoopsDatabase {
 	 * @var int
 	 */
 	var $rowCount = 0;
-	
+
 	var $allowWebChanges = false;
 
 	public function connect($selectdb = true) {
@@ -69,12 +69,15 @@ class XoopsPDODatabase extends XoopsDatabase {
 			$sql = $this->convertToSQLServerSyntax($sql);
 			$pdoStatement = $this->conn->prepare("$sql", array(PDO::ATTR_CURSOR=>PDO::CURSOR_SCROLL)); // should now return a $result for which the rowCount will be accurate
 			if(!$pdoStatement) {
-			    $this->logger->addQuery($sql, $this->error(), $this->errno());
-				//print "Prepare failed for this SQL:<br>$sql<br>SQL Server reports: ".$this->error()."<br>";
+				$this->logger->addQuery($sql, $this->error(), $this->errno());
 			}
 			$result = $pdoStatement->execute();
 			if(!$result) {
-			   $this->logger->addQuery($sql, $this->error(), $this->errno());
+				$this->logger->addQuery($sql, $this->error(), $this->errno());
+				// temp hack to crash the code when a queries fails
+				icms_debug('query failing: ' . $sql);
+				debug_print_backtrace() ;
+				exit;
 			} else {
 				$this->logger->addQuery($sql);
 			}
@@ -119,96 +122,98 @@ class XoopsPDODatabase extends XoopsDatabase {
 	}
 
 	private function convertToSQLServerSyntax($sql) {
-	    // change SHOW COLUMNS to the SQL Server equivalent
-	    if(substr($sql, 0, 12)=="SHOW COLUMNS") {
-		// need to get the table name and then insert it as appropriate in the replacement SQL syntax
-		// need to get the LIKE param and handle that too
-		$fromPos = strpos($sql, "FROM");
-		$tableName = substr($sql, $fromPos+5);
-		$sql = "SELECT column_name FROM information_schema.columns WHERE table_name='" . trim($tableName) . "'";
-		if($likePos = strpos($tableName, "LIKE")) {
-		    $likeName = substr($tableName, $likePos+5);
-		    $sql .= " AND column_name = '".trim($likeName)."'";
-		} 
-	    }
-	    if(trim($sql) == "SHOW TABLES") {
-		// convert to get table names from the information schema
-		$sql = "SELECT table_name FROM information_schema.tables";
-	    }
-	    // replace backticks with [ ]
-	    $replacement = "[";
-	    $backtickPos = 0;
-	    while($backtickPos = strpos($sql, "`", $backtickPos+1)) {
-		$sql = substr_replace($sql, $replacement,$backtickPos,1);
-		$replacement = $replacement == "[" ? "]" : "[";
-	    }
-	    
-	    // check for a limit clause and if found, send to the necessary function
-	    // assume all caps LIMIT space and a number near the end of the statement is the actual limit clause that we need to work with
-	    if($limitPos = strrpos($sql, "LIMIT")) {
-		// verify it's at the end
-		// check for comma
-		// if one value, then it's the limit, if two, second is limit, first is offset
-		if($limitPos >= strlen($sql) - 10) {// if the limit is pretty much at the end of the sql....
-        	    $limitClause = substr($sql, $limitPos);
-		    $sql = substr($sql, 0, $limitPos);
-		    if($commaPos = strpos($limitClause, ",")) {
-			$limit = intval(substr($limitClause,$commaPos+1));
-			$offset = intval(str_replace("LIMIT", "", substr($limitClause, 0, $commaPos)));
-		    } else {
-			$limit = intval(str_replace("LIMIT", "", $limitClause));
-			$offset = 0;
-		    }
+		// change SHOW COLUMNS to the SQL Server equivalent
+	  
+		icms_debug($sql);
+		if(substr($sql, 0, 12)=="SHOW COLUMNS") {
+			// need to get the table name and then insert it as appropriate in the replacement SQL syntax
+			// need to get the LIKE param and handle that too
+			$fromPos = strpos($sql, "FROM");
+			$tableName = substr($sql, $fromPos+5);
+			$sql = "SELECT column_name FROM information_schema.columns WHERE table_name='" . trim($tableName) . "'";
+			if($likePos = strpos($tableName, "LIKE")) {
+				$likeName = substr($tableName, $likePos+5);
+				$sql .= " AND column_name = '".trim($likeName)."'";
+			}
 		}
-		$sql = $this->limit_to_top_n($sql, $offset, $limit);
-	    }
-	    return $sql;
+		if(trim($sql) == "SHOW TABLES") {
+			// convert to get table names from the information schema
+			$sql = "SELECT table_name FROM information_schema.tables";
+		}
+		// replace backticks with [ ]
+		$replacement = "[";
+		$backtickPos = 0;
+		while($backtickPos = strpos($sql, "`", $backtickPos+1)) {
+			$sql = substr_replace($sql, $replacement,$backtickPos,1);
+			$replacement = $replacement == "[" ? "]" : "[";
+		}
+	  
+		// check for a limit clause and if found, send to the necessary function
+		// assume all caps LIMIT space and a number near the end of the statement is the actual limit clause that we need to work with
+		if($limitPos = strrpos($sql, "LIMIT")) {
+			// verify it's at the end
+			// check for comma
+			// if one value, then it's the limit, if two, second is limit, first is offset
+			if($limitPos >= strlen($sql) - 10) {// if the limit is pretty much at the end of the sql....
+				$limitClause = substr($sql, $limitPos);
+				$sql = substr($sql, 0, $limitPos);
+				if($commaPos = strpos($limitClause, ",")) {
+					$limit = intval(substr($limitClause,$commaPos+1));
+					$offset = intval(str_replace("LIMIT", "", substr($limitClause, 0, $commaPos)));
+				} else {
+					$limit = intval(str_replace("LIMIT", "", $limitClause));
+					$offset = 0;
+				}
+			}
+			$sql = $this->limit_to_top_n($sql, $offset, $limit);
+		}
+		return $sql;
 	}
 
 	// thanks to moodle for this code
 	private function limit_to_top_n($sql, $offset, $limit) {
-	    if ($limit < 1 && $offset < 1) {
-		return $sql;
-	    }
-	    $limit = max(0, $limit);
-	    $offset = max(0, $offset);
-    
-	    if ($limit > 0 && $offset == 0) {
-		$sql1 = preg_replace('/^([\s(])*SELECT( DISTINCT | ALL)?(?!\s*TOP\s*\()/i',
+		if ($limit < 1 && $offset < 1) {
+			return $sql;
+		}
+		$limit = max(0, $limit);
+		$offset = max(0, $offset);
+
+		if ($limit > 0 && $offset == 0) {
+			$sql1 = preg_replace('/^([\s(])*SELECT( DISTINCT | ALL)?(?!\s*TOP\s*\()/i',
 		    "\\1SELECT\\2 TOP $limit", $sql);
-	    } else {
-		// Only apply TOP clause if we have any limitnum (limitfrom offset is hadled later)
-		if ($limit < 1) {
-		   $limit = "9223372036854775806"; // MAX BIGINT -1
-		}
-		if (preg_match('/\w*FROM[\s|{]*([\w|.]*)[\s|}]?/i', $sql, $match)) {
-		    $from_table = $match[1];
-		    if (preg_match('/SELECT[\w|\s]*(\*)[\w|\s]*FROM/i', $sql)) {
-			/*
-			// Nov 17 2010 - I don't get why this is necessary, why not just use the columns as declared in the original statement?!
-			// Need all the columns as the emulation returns some temp cols
-			$cols = array_keys($this->get_columns($from_table));
-			$cols = implode(', ', $cols);
-			*/
-			$fromPos = strpos($sql, "FROM");
-			$cols = str_replace("SELECT", "", substr($sql, 0, $fromPos-1));
-		    } else {
-			$cols = '*';
-		    }
-		    $sql1 = 'SELECT '.$cols.' FROM ( '
-			.'SELECT sub2.*, ROW_NUMBER() OVER(ORDER BY sub2.line2) AS line3 FROM ( '
-			.'SELECT 1 AS line2, sub1.* FROM '
-			.''.$from_table.' AS sub1 '
-			.') AS sub2 '
-			.') AS sub3 '
-			.'WHERE line3 BETWEEN '.($offset+1).' AND '
-			.($offset + $limit);
 		} else {
-		    $sql1 = "SELECT 'Invalid table'";
+			// Only apply TOP clause if we have any limitnum (limitfrom offset is hadled later)
+			if ($limit < 1) {
+				$limit = "9223372036854775806"; // MAX BIGINT -1
+			}
+			if (preg_match('/\w*FROM[\s|{]*([\w|.]*)[\s|}]?/i', $sql, $match)) {
+				$from_table = $match[1];
+				if (preg_match('/SELECT[\w|\s]*(\*)[\w|\s]*FROM/i', $sql)) {
+					/*
+					 // Nov 17 2010 - I don't get why this is necessary, why not just use the columns as declared in the original statement?!
+					 // Need all the columns as the emulation returns some temp cols
+					 $cols = array_keys($this->get_columns($from_table));
+					 $cols = implode(', ', $cols);
+					 */
+					$fromPos = strpos($sql, "FROM");
+					$cols = str_replace("SELECT", "", substr($sql, 0, $fromPos-1));
+				} else {
+					$cols = '*';
+				}
+				$sql1 = 'SELECT '.$cols.' FROM ( '
+				.'SELECT sub2.*, ROW_NUMBER() OVER(ORDER BY sub2.line2) AS line3 FROM ( '
+				.'SELECT 1 AS line2, sub1.* FROM '
+				.''.$from_table.' AS sub1 '
+				.') AS sub2 '
+				.') AS sub3 '
+				.'WHERE line3 BETWEEN '.($offset+1).' AND '
+				.($offset + $limit);
+			} else {
+				$sql1 = "SELECT 'Invalid table'";
+			}
 		}
-	    }
-    
-	    return $sql1;
+
+		return $sql1;
 	}
 
 }
@@ -216,15 +221,15 @@ class XoopsPDODatabase extends XoopsDatabase {
 class XoopsPDODatabaseSafe extends XoopsPDODatabase
 {
 
-  /**
-   * perform a query on the database
-   *
-   * @param string $sql a valid MySQL query
-   * @param int $limit number of records to return
-   * @param int $start offset of first record to return
-   * @return resource query result or FALSE if successful
-   * or TRUE if successful and no result
-   */
+	/**
+	 * perform a query on the database
+	 *
+	 * @param string $sql a valid MySQL query
+	 * @param int $limit number of records to return
+	 * @param int $start offset of first record to return
+	 * @return resource query result or FALSE if successful
+	 * or TRUE if successful and no result
+	 */
 	function query($sql, $limit=0, $start=0)
 	{
 		return $this->queryF($sql, $limit, $start);
@@ -248,6 +253,6 @@ class XoopsPDODatabaseSafe extends XoopsPDODatabase
 class XoopsPDODatabaseProxy extends XoopsPDODatabase
 {
 
-  
+
 }
 
