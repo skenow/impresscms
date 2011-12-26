8bc5e8736c217379288b0608ebb372a7572ff413
Steve Kenow <skenow@impresscms.org>
2011-12-26 22:57:44 +0000

    Updating version and release info for 1.3.1 Final

--- htdocs/include/version.php
+++ htdocs/include/version.php
@@ -10,7 +10,7 @@
  * @version		$Id$
  */
 
-define('ICMS_VERSION_NAME', 'ImpressCMS 1.3.1 RC');
+define('ICMS_VERSION_NAME', 'ImpressCMS 1.3.1 Final');
 
 // For backward compatibility with XOOPS
 define('XOOPS_VERSION', ICMS_VERSION_NAME);
@@ -23,15 +23,15 @@ define('XOOPS_VERSION', ICMS_VERSION_NAME);
  * 10 = Final
  */
 
-define('ICMS_VERSION_STATUS', 3);
+define('ICMS_VERSION_STATUS', 10);
 
 /**
  * Build number
  *
  * Every release has its own build number, incrementable by 1 everytime we make a release
  */
-// impresscms_1.2.7 RC = 52
-define('ICMS_VERSION_BUILD', 53);
+// impresscms_1.2.7 Final = 54
+define('ICMS_VERSION_BUILD', 55);
 
 /**
  * Latest dbversion of the System Module
