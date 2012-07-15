6067be64f98823133fe77bff4b46d744671642f8
Steve Kenow <skenow@impresscms.org>
2012-07-15 20:47:47 +0000

    Updating changelog, version and release notes for a beta release

--- htdocs/include/version.php
+++ htdocs/include/version.php
@@ -10,7 +10,7 @@
  * @version		$Id$
  */
 
-define('ICMS_VERSION_NAME', 'ImpressCMS 1.3.3 RC');
+define('ICMS_VERSION_NAME', 'ImpressCMS 1.3.3 Beta');
 
 // For backward compatibility with XOOPS
 define('XOOPS_VERSION', ICMS_VERSION_NAME);
@@ -23,14 +23,14 @@ define('XOOPS_VERSION', ICMS_VERSION_NAME);
  * 10 = Final
  */
 
-define('ICMS_VERSION_STATUS', 3);
+define('ICMS_VERSION_STATUS', 2);
 
 /**
  * Build number
  *
  * Every release has its own build number, incrementable by 1 everytime we make a release
  */
-// impresscms_1.3.3 RC = 63
+// impresscms_1.3.3 Beta = 63
 define('ICMS_VERSION_BUILD', 63);
 
 /**
