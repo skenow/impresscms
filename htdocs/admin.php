2b7666adfb0e46ed41a0d401707261f5975bb00d
David Janssens <david.j@impresscms.org>
2013-03-18 13:25:11 +0100

    Restore Xoops License headers to original form where needed Re #687

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -1,4 +1,5 @@
 <?php
+<?php
 /**
  * Admin control panel entry page
  *
@@ -13,10 +14,37 @@
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  * @package		core
  * @since		XOOPS
- * @author		http://www.xoops.org The XOOPS Project
+ * @author		Taiwen Jiang (phppp or D.J.) <php_pp@hotmail.com>
  * @author		modified by marcan <marcan@impresscms.org>
- * @version		$Id$
+ * @version		$Id: admin.php 12142 2012-12-30 13:13:59Z fiammy $
  */
+
+// $Id: admin.php 12142 2012-12-30 13:13:59Z fiammy $
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                    Copyright (c) 2000 XOOPS.org                           //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
 
 define('ICMS_IN_ADMIN', 1);
 
--- htdocs/image.php
+++ htdocs/image.php
@@ -2,12 +2,41 @@
 /**
  * functions for image.
  *
+ * @copyright	http://www.xoops.org/ The XOOPS Project
+ * @copyright	XOOPS_copyrights.txt
  * @copyright	http://www.impresscms.org/ The ImpressCMS Project
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  * @package		core
  * @since		XOOPS
- * @version		$Id$
+ * @author		skalpa <psk@psykaos.net>
+ * @version		$Id: image.php 12142 2012-12-30 13:13:59Z fiammy $
  */
+
+// $Id: image.php 12142 2012-12-30 13:13:59Z fiammy $
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                    Copyright (c) 2000 XOOPS.org                           //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
 
 $image_id = isset($_GET["id"]) ? (int) $_GET["id"] : 0;
 if (empty($image_id)) {
